c74795470da20aaf536e1fbd038d0ed2d1d85899
Nesuki <49078396+Nesuki@users.noreply.github.com>
2019-04-19 16:59:06 -0300

    more practice
    first practice with text files on python

---
+++ fileinputoutput.py
@@ -0,0 +1,9 @@
+#This exercise will require several lines of code.
+#Write a script that opens a file named 'test.txt', writes 'Hello World' to the file, then closes it
+
+
+
+
+me = open('test.txt', 'w')
+me.write('Hello World')
+me.close()
