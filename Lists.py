7d2e53ffb6ade4bb84436897f5b1fb713414b0a7
Nesuki <49078396+Nesuki@users.noreply.github.com>
2019-04-19 15:49:08 -0300

    listconcoction
    first list practice

---
+++ Lists.py
@@ -0,0 +1,3 @@
+#create a list that contains at least one string, one integrer and one float, the answer should just be one list on a single line, don't assign a variable name to the list
+
+[9, 'nine', 24.5]
