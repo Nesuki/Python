da85d6ffb0e93cfa998a075ad7c69a08151abab0
Nesuki <49078396+Nesuki@users.noreply.github.com>
2019-04-19 14:55:54 -0300

    Create PrintFormatting.py
    first printformattin exercise

---
+++ PrintFormatting.py
@@ -0,0 +1,5 @@
+#Write an expression using any of the string formatting methods we have learned to return the phrase 'Python rules!'
+
+#Your solution should be entered on one line, you can not use variable names, only the strings themselves.
+
+print ('Python {}'.format('rules!'))
